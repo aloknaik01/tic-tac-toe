15ccc73b598c518c9224c4e4748f20e3136af6bf
aloknaik <aloknaik016261@gmail.com>
2025-07-05 19:45:22 +0530

    ADDED

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import ModeSelector from "./components/ModeSelector/ModeSelector";
 function App() {
-  const [count, setCount] = useState(0)
+  const [mode, setMode] = useState(null);
 
+  function handleSelectMode(selectedMode) {
+    console.log("User selected mode:", selectedMode);
+    setMode(selectedMode);
+  }
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {!mode && <ModeSelector onSelectMode={handleSelectMode} />}
+        {mode && <h2>You chose: {mode}</h2>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
