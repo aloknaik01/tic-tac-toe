ff45c36b5102f3fa6685fddcf909b9cd7499cac1
aloknaik <aloknaik016261@gmail.com>
2025-07-11 14:31:46 +0530

    full guide to integrate 5. Setup Dark/Light Mode Switch from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import { useState } from "react";
 import ModeSelector from "./components/ModeSelector/ModeSelector";
 import ColorTest from "./components/ui/ColorTest";
+import DarkModeToggle from "./components/ui/DarkModeToggle";
 function App() {
   const [mode, setMode] = useState(null);
+  const [dark, setDark] = useState(true);
 
   function handleSelectMode(selectedMode) {
     console.log("User selected mode:", selectedMode);
     setMode(selectedMode);
+
+    //Toggle Dark and Light Mode
+    const toggle = () => {
+      document.documentElement.classList.toggle("dark");
+      setDark(!dark);
+    };
   }
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold bg-red-400 underline">
-          Hello world!
-        </h1>
-        <div className="glass p-4">Glassmorphism content</div>
-        {!mode && <ModeSelector onSelectMode={handleSelectMode} />}
-        {mode && <h2>You chose: {mode}</h2>}
+      <div className="min-h-screen flex flex-col items-center justify-center gap-4">
+        <h1 className="text-4xl font-bold text-neonPink">Tic Tac Toe</h1>
+        <DarkModeToggle />
+        <div
+          className="
+  p-4 rounded
+  bg-white dark:bg-darkBg
+  text-black dark:text-white
+"
+        >
+          Hello Dark Mode!
+        </div>
+
+        <div className="
+  p-6 rounded-lg
+  bg-neonPink text-white
+  dark:bg-neonBlue dark:text-black
+">
+  Neon Button
+</div>
       </div>
       <ColorTest />
     </>
