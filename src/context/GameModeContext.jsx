2f42974414db5e5867052392426c873379aac374
aloknaik <aloknaik016261@gmail.com>
2025-07-11 15:44:49 +0530

    feat: add GameModeSelector component

--- src/context/GameModeContext.jsx
+++ src/context/GameModeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const GameModeContext = createContext();
 
 export const GameModeProvider = ({ children }) => {
-  const [gameMode, setGameMode] = useState(null);
+  const [gameMode, setGameMode] = useState("pvp");
 
   return (
     <GameModeContext.Provider value={{ gameMode, setGameMode }}>
